ea2c4749816bdbf17d8d0e9629ee4b8bece68a2c
jonathanboreljaquet <jonathan.borel-jaquet@hotmail.com>
2025-05-07 17:47:12 +0200

    Add .gitignore

--- Mattejon_smart-contract.sol
+++ Mattejon_smart-contract.sol
@@ -3,56 +3,29 @@
 pragma solidity ^0.8.27;
 
 import {ERC721} from "@openzeppelin/contracts@5.3.0/token/ERC721/ERC721.sol";
-import {ERC721Enumerable} from "@openzeppelin/contracts@5.3.0/token/ERC721/extensions/ERC721Enumerable.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts@5.3.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import {Ownable} from "@openzeppelin/contracts@5.3.0/access/Ownable.sol";
 
-contract Mattejon is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    constructor(address initialOwner)
-        ERC721("Mattejon", "MJTK")
-        Ownable(initialOwner)
-    {}
+contract Mattejon is ERC721URIStorage {
+    mapping(address => bool) public isOwner;
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
+    constructor(address secondOwner)
+        ERC721("Mattejon", "MJTK") 
     {
+        isOwner[msg.sender] = true;   // Moi le créateur J.BJ
+        isOwner[secondOwner] = true;  // Je définis le deuxième propriétaire, M.S
+    }
+
+    //Fonction pour vérifier si l'utilisateur est un Owner
+    modifier onlyOwner() {
+        require(isOwner[msg.sender], "Not authorized");
+        _;
+    }
+
+    // Fonction pour mint un NFT
+    //https://emn178.github.io/online-tools/keccak_256_checksum.html site pour hash une image avec fonction de hachage Keccak-256
+    function safeMint(address to, uint256 tokenId, string memory uri) public onlyOwner {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
-    }
-
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
